db6445f2834a07ece002f60d98e49df2edbb86b2
Anjelica <ambrosio.anjelica@gmail.com>
2019-10-15 14:56:00 -0700

    added stats

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-#Reika's Game (Name to be Decided)
+#Search by Moonlight
 import random
 
 #Abilities
@@ -13,15 +13,42 @@ class Ability:
         #Return the Value
         return self.attack
 
-# Weapon
+# Weapons
 class Weapon(Ability):
     def __init__(self, name, type, strength):
+        super().__init__(name, attack_value)
         self.name = weapon_name
         self.type = weapon_type
         #Inital Strength
         self.strength = 10
 
-clas
+    def attack(self):
+        self.attack_value = random.randint(0, 1500)
+        # TODO: Use what you learned to complete this method.
+        damage = random.randint(0, self.attack_value)
+        return damage // 2
+
+class Armor:
+    def __init__(self, name, type, protection):
+        self.name = armor_name
+        self.type = armor_type
+        #Initial Protection
+        self.protection = 10
+
+class Actor:
+    def __init__(self, name, age, starting_hp, starting_atk, starting_def):
+        self.name = actor_name
+        self.age = actor_age
+        #Stats
+        self.starting_hp = starting_hp
+        self.starting_atk = starting_atk
+        self.starting_def = starting_def
+    
+
+
+
+
+
 
 
 
@@ -37,4 +64,5 @@ clas
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
+    #Exposition
     print("Welcome!")
