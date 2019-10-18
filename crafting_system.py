eaab690a833cc6fafb57739e2b7366fec836b76c
Anjelica <ambrosio.anjelica@gmail.com>
2019-10-18 13:36:32 -0700

    added crafting system

---
+++ crafting_system.py
@@ -0,0 +1,11 @@
+from classes import *
+
+#Step 1
+def Gather():
+    #Test
+    item_amount == 5
+    item_name == "Iron Ore"
+    print("Gathering resources...")
+    print("Collected {0} {1}!".format(item_amount, item_name))
+    self.add_item("Iron Ore", "Ore", 0)
+    
